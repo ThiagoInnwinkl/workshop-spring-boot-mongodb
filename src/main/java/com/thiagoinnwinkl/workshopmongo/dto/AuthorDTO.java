2aad0073f8fd4a8324cc98a3a983233f6f1f1fd7
Thiago Innwinkl <thiago10innwinkl@gmail.com>
2022-06-10 10:26:53 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/thiagoinnwinkl/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,41 @@
+package com.thiagoinnwinkl.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.thiagoinnwinkl.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String nome;
+	
+	public AuthorDTO() {
+		
+	}
+	
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
