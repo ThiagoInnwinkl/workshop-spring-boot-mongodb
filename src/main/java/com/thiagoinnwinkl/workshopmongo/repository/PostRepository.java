834a4b43ef57d489d9ecd982ed683fc39af366bf
Thiago Innwinkl <thiago10innwinkl@gmail.com>
2022-06-10 00:49:01 -0300

    Criando endpoint com user aninhado

--- src/main/java/com/thiagoinnwinkl/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/thiagoinnwinkl/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.thiagoinnwinkl.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.thiagoinnwinkl.workshopmongo.domain.User;
+import com.thiagoinnwinkl.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {	
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
